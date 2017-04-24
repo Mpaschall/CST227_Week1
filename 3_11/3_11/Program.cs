9fccb4f105fd392bc3bff22a7efcf882f04aebf7
Mathew Paschall <XDarkZero89@hotmail.com>
2017-04-23 21:57:11 -0400

    Add files via upload

---
+++ 3_11/3_11/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cerealA = 0;
+            double cerealB = 0;
+            double cheapCereal = 0;
+            double milkA = 0;
+            double milkB = 0;
+            double cheapMilk = 0;
+            double cost = 0;
+
+            Console.WriteLine("Enter the cereal price at store A");
+            cerealA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the cereal price at store B");
+            cerealB = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the milk price at store A");
+            milkA = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the milk price at store B");
+            milkB = Convert.ToDouble(Console.ReadLine());
+
+            if (cerealA < cerealB)
+            {
+                cheapCereal = cerealA;
+            }
+            else
+            {
+                cheapCereal = cerealB;
+            }
+
+            if (milkA < milkB)
+            {
+                cheapMilk = milkA;
+            }
+            else
+            {
+                cheapMilk = milkB;
+            }
+
+            cost = ((3 * cheapCereal) + (2 * cheapMilk));
+            Console.WriteLine("The cheapest cost for these items is $" + cost);
+        }
+    }
+}
---
+++ 3_9/3_9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pay = 7.50;
+            double oT = 11.25;
+            double hours = 0;
+            double payedMoney = 0;
+
+            Console.WriteLine("Enter the number of hours worked");
+            hours = Convert.ToDouble(Console.ReadLine());
+            if (hours < 40)
+            {
+                payedMoney = pay * hours;
+                Console.WriteLine("Your pay is " + payedMoney);
+            }
+            else if (hours > 40)
+            {
+                payedMoney = (((hours - 40) * 11.25) + (40 * pay));
+                Console.WriteLine("Your pay is " + payedMoney);
+            }
+        }
+    }
+}
