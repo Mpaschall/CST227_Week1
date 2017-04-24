24fafb7acf66cdf85da306f16d290b3901f0bb72
Mathew Paschall <XDarkZero89@hotmail.com>
2017-04-23 21:54:50 -0400

    Add files via upload

---
+++ 3_15/3_15/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            double grade = 0;
+            double totalGrade = 0;
+            double gPA = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                Console.WriteLine("Please enter a test score. -1 to exit");
+                grade = Convert.ToDouble(Console.ReadLine());               
+
+                if (grade < 0)
+                {
+                    break;
+                }
+                totalGrade += grade;
+                count++;
+            }
+            double avgGrade = (totalGrade / count);
+            if (avgGrade >= 90 && avgGrade <= 100)
+            {
+                gPA = 4.0;
+            }
+            else if (avgGrade >= 80 && avgGrade <= 89.99)
+            {
+                gPA = 3.0;
+            }
+            else if (avgGrade >= 70 && avgGrade <= 79.99)
+            {
+                gPA = 2.0;
+            }
+            else if (avgGrade >= 60 && avgGrade <= 69.99)
+            {
+                gPA = 1.0;
+            }
+            else if (avgGrade >= 0 && avgGrade <= 59.99)
+            {
+                gPA = 0;
+            }
+
+            Console.WriteLine("Your GPA is " + gPA);
+        }
+    }
+}
---
+++ 3_17/3_17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the number of integers you would like to compare.");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[x];
+
+            for (int i = 0; i < x; i++)
+            {
+                Console.WriteLine("Please enter a number to compare.");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The largest number is " + array.Max());
+        }
+    }
+}
